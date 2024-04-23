51a2e3b15e833f0e865a4ac7577a5d37c0f1fa30
Alexis <alexis.contreras01@alumnos.ucn.cl>
2024-04-23 01:13:19 -0400

    Deleting no used readme files

--- src/Presentation/screens/login/LoginScreen.tsx
+++ src/Presentation/screens/login/LoginScreen.tsx
@@ -51,13 +51,6 @@ const LoginScreen = ({ navigation,route }: Props) => {
           </Pressable>
         </View>
 
-        <View style={[styles.signUpContainer, {top: 625}]}>
-          <Pressable  onPress={() => navigation.navigate('Category') }>
-            <Text style={styles.signUpLink}>TEST CATEGORY</Text>
-          </Pressable>
-        </View>
-
-        
     </View>
     
   )
