ddc48066862a579ed2a7e561c24da211f6f1421b
Royman Valencia <royman.valencia@alumnos.ucn.cl>
2024-04-27 22:02:05 -0400

    add: Category CRUD folders 2

---
+++ src/Presentation/screens/admin/category/create/CategoryCreateScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CategoryCreateScreen = () => {
+  return (
+    <View>
+      <Text>CategoryCreateScreen</Text>
+    </View>
+  )
+}
+
+export default CategoryCreateScreen
