d4fc1a7537c76f0b2b192be7d13c00a2f3a1f839
Giantt2 <giantt67@gmail.com>
2024-04-29 22:53:36 -0400

    feat(categoryList): Scrollview & individual category template implemented

--- src/Presentation/screens/admin/category/list/CategoryListScreen.tsx
+++ src/Presentation/screens/admin/category/list/CategoryListScreen.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
 import styles from './Styles';
 import { Pressable } from 'react-native'
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../../../../navigation/MainAppStack';
+import { LinearGradient } from 'expo-linear-gradient';
 
 interface Props extends StackScreenProps<RootStackParamList, 'CategoryListScreen'> {}
 
@@ -11,6 +12,44 @@ export const CategoryListScreen = ({ navigation,route }: Props) => {
   return (
     <View style={styles.CategoryListContainer}>
       <Text style={styles.CategoryListText}>CATEGORÍAS</Text>
+
+      <ScrollView style={styles.categoryListInnerContainer} showsVerticalScrollIndicator = {false}>
+
+      <LinearGradient
+        colors={['#262B33', 'transparent']}
+        style={styles.categoryListElement}>
+        <View style={styles.categoryListElement}>
+          <Image style={styles.categoryListUserImage} source={require('../../../../../../assets/images/chicken-skewers-on-a-plate.png')} />
+          <Text style={styles.categoryListElementText}>Hamburguesa</Text>
+
+          <View style={styles.buttomEdit}> 
+            <Pressable onPress={() => navigation.goBack()}> 
+              <Text style={styles.editText}>Editar producto</Text>
+            </Pressable>
+          </View>
+        </View>
+      </LinearGradient>
+
+      <LinearGradient
+        colors={['#262B33', 'transparent']}
+        style={styles.categoryListElement}>
+        <View style={styles.categoryListElement}>
+          <Image style={styles.categoryListUserImage} source={require('../../../../../../assets/images/chicken-skewers-on-a-plate.png')} />
+          <Text style={styles.categoryListElementText}>Pizza</Text>
+
+          <View style={styles.buttomEdit}> 
+            <Pressable onPress={() => navigation.goBack()}> 
+              <Text style={styles.editText}>Editar producto</Text>
+            </Pressable>
+          </View>
+        </View>
+      </LinearGradient>
+
+        
+
+
+
+      </ScrollView>
 
 
       <View style={styles.buttomSave}> 
--- src/Presentation/screens/admin/category/list/Styles.tsx
+++ src/Presentation/screens/admin/category/list/Styles.tsx
@@ -13,7 +13,7 @@ const CategoryListStyles = StyleSheet.create(
 
       CategoryListText: {
         position: 'absolute',
-        top: "19%",
+        top: "15%",
         color: 'white',
         fontFamily: 'Poppins',
         fontSize: 20,
@@ -24,29 +24,87 @@ const CategoryListStyles = StyleSheet.create(
 
       buttomSave: {
         position: 'absolute',
-        top: "77%",
+        top: "79%",
         backgroundColor: '#D17842',
         borderRadius: 10,
         width: 150,
-        height: 40,
+        height: 45,
       },
 
       buttomAdd: {
         position: 'absolute',
-        top: "85%",
+        top: "87%",
         backgroundColor: '#D17842',
         borderRadius: 10,
         width: 150,
-        height: 40,
+        height: 45,
       },
      
       SaveText: {
         color: 'white',
         fontFamily: 'Poppins',
         textAlign: 'center',
-        marginTop: 10,
+        marginTop: 11,
       },
 
+      categoryListInnerContainer: {
+        flex: 1,
+        flexGrow: 1,
+        flexDirection: 'column',
+        width: "85%",
+        height: "52%",
+        position: 'absolute',
+        top: "23%",
+        alignContent: 'center',
+        borderRadius: 10,
+      },
+
+        categoryListElement: {
+            //backgroundColor: '#262B33',
+            width: "100%",
+            height: 140,
+            borderRadius: 30,
+            marginBottom: 10,
+            paddingStart: 15,
+            paddingEnd: 15,
+            alignSelf: 'stretch',
+        },
+
+        categoryListElementText: {
+            color: 'white',
+            fontFamily: 'Poppins',
+            textAlign: 'left',
+            marginTop: '8%',
+            marginLeft: '35%',
+            fontSize: 18,
+        },
+
+        categoryListUserImage: {
+            borderRadius: 30,
+            width: 100,
+            height: 100,
+            position: 'absolute',
+            top: "12.5%",
+          },
+
+          buttomEdit: {
+            position: 'absolute',
+            top: "55%",
+            left: "47%",
+            backgroundColor: 'transparent',
+            borderColor: '#D17842',
+             borderWidth: 2,
+            borderRadius: 10,
+            width: 150,
+            height: 40,
+          },
+
+          editText: {
+            color: '#D17842',
+            fontFamily: 'Poppins',
+            textAlign: 'center',
+            marginTop: '5%',
+          },
 })
 
 export default CategoryListStyles;
