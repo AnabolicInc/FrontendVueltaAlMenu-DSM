dd7b88ecec1740ac3933a619bb5cd7abc880de51
Giantt2 <giantt67@gmail.com>
2024-05-06 03:34:06 -0400

    feat(profile): edit profile implemented

--- src/Presentation/screens/profile/info/ViewModel.tsx
+++ src/Presentation/screens/profile/info/ViewModel.tsx
@@ -1,14 +1,16 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../../context/auth/AuthContext";
 import { RemoveUserUseCase } from "../../../../Domain/useCases/UserLocal/RemoveUserLocal";
 
 const ProfileInfoViewModel = () => {
+
     const { user, logout } = useContext(AuthContext);
 
     const logoutUser = async () => {
         await RemoveUserUseCase();
         logout();
     }
+
     return { 
         user,
         logoutUser
--- src/Presentation/screens/profile/update/ProfileUpdateScreen.tsx
+++ src/Presentation/screens/profile/update/ProfileUpdateScreen.tsx
@@ -1,11 +1,131 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Pressable, Image, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
-const ProfileUpdateScreen = () => {
+import styles from './Styles'
+import useViewModel from './ViewModel'
+import { UserInfo } from '../../../components/UserInfo'
+import { ScrollView, TextInput } from 'react-native-gesture-handler'
+import { StackScreenProps } from '@react-navigation/stack'
+import { RootStackParamList } from '../../../navigation/MainAppStack'
+import { ModalPickImage } from '../../../components/ModalPickImage'
+
+interface Props extends StackScreenProps<RootStackParamList, 'ProfileUpdateScreen'> {};
+
+const ProfileUpdateScreen = ({ navigation }: Props) => {
+
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+
+  const {
+    name,
+    lastName,
+    image,
+    phone,
+    errorMessages,
+    file,
+    loading,
+    onChange,
+    onChangeInfoUpdate,
+    pickImage,
+    takePhoto,
+    updateUser,
+    user
+  } = useViewModel();
+
+  useEffect(() => {
+    onChangeInfoUpdate(user.name, user.lastName, user.phone)
+  }, [user])
+
+
   return (
-    <View>
-      <Text>ProfileUpdateScreen</Text>
-    </View>
+    <View style={styles.profileContainer}>
+		
+		<Text style={styles.mainText}>Editar perfil</Text>
+		<ScrollView style={styles.profileInnerContainer}>
+
+      <View style={styles.profileUpdateUserImageContainer}>
+        <TouchableOpacity 
+        onPress={() => setModalVisible(true)}>
+          {
+            image == ''
+            ?
+            <View>
+              <Image
+              style={styles.profileUpdateUserImage}
+              source = {{uri: user?.image}}
+              />
+              <Text style={styles.userImageText}>Seleccione una imagen</Text>
+                {
+                  errorMessages.image && (
+                    <Text style={{
+                      ...styles.userText, marginTop: 10, backgroundColor: '#ff7f7f', borderLeftWidth: 3,
+                      borderColor: '#993235',
+                      color: 'white',
+                      fontSize: 14,
+                      fontWeight: '600',
+                      marginVertical: 12,
+                      paddingVertical: 8,
+                      paddingHorizontal: 12,
+                    }}>{errorMessages.image}</Text>
+                  )
+                }
+            </View>
+            :
+            <Image
+            style={styles.logo}
+              source={{ uri: image }}
+            />
+          }
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.inputContainer}>
+			<TextInput
+        style={styles.textInputContainer}
+        placeholder="Nombre"
+        value={name}
+        onChangeText={(text) => onChange('name', text)}
+      />
+
+      {errorMessages.name && <Text style={styles.errorText}>{errorMessages.name}</Text>}
+
+      <TextInput
+        style={styles.textInputContainer}
+        placeholder="Apellido"
+        value={lastName}
+        onChangeText={(text) => onChange('lastName', text)}
+      />
+      {errorMessages.lastName && <Text style={styles.errorText}>{errorMessages.lastName}</Text>}
+
+      <TextInput
+        style={styles.textInputContainer}
+        placeholder="Teléfono"
+        keyboardType='number-pad'
+        value={phone}
+        onChangeText={(text) => onChange('phone', text)}
+      />
+      {errorMessages.phone && <Text style={styles.errorText}>{errorMessages.phone}</Text>}
+
+      <Pressable onPress={() => updateUser()}>
+        <Text style={styles.editButton}>Guardar</Text>
+          
+      </Pressable>
+
+      </View>
+		</ScrollView>
+    
+    <ModalPickImage
+    openGallery={pickImage}
+    openCamera={takePhoto}
+    modalUseState={modalVisible}
+    setModalUseState={setModalVisible}
+    />
+
+    {
+      loading && (
+        <ActivityIndicator style={styles.loading} size={"large"} color={'#D17842'} />
+      )
+    }
+  </View>
   )
 }
 
