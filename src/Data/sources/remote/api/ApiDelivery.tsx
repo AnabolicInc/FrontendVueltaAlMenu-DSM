cb1e1735559ee119391e3cbf3323ae5dce7e668b
Daniel <daniel.rojas02@alumnos.ucn.cl>
2024-05-09 00:19:35 -0400

    add: implement missing features

--- src/Data/sources/remote/api/ApiDelivery.tsx
+++ src/Data/sources/remote/api/ApiDelivery.tsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import { LOCAL_HOST } from "@env";
 
 const ApiDelivery = axios.create({
-    baseURL: LOCAL_HOST,
+    // baseURL: LOCAL_HOST,
+    baseURL: 'http://10.0.2.2:8080/api/', // Para android studio
     headers: {
         'Content-type': 'application/json'
     }
