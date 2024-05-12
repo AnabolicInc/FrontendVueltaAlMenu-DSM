92365401a4cdddb8a367e16fd6bcfead6479c300
tortaaaa <pablo.reyes01@alumnos.ucn.cl>
2024-05-12 01:48:48 -0400

    feat: ChangePasswordScreen structure implemented

--- src/Presentation/screens/profile/changePassword/ChangePasswordScreen.tsx
+++ src/Presentation/screens/profile/changePassword/ChangePasswordScreen.tsx
@@ -1,20 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { StackScreenProps } from '@react-navigation/stack'
-import { RootStackParamList } from '../../../navigation/MainAppStack'
-import { RootAdminBottomTabParamList } from '../../../navigation/tabs/admin/AdminBottomTab'
-
+import React, { useState } from 'react';
+import { View, Text, TextInput, Pressable } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
+import { RootAdminBottomTabParamList } from '../../../navigation/tabs/admin/AdminBottomTab';
+import ProfileInfoStyles from './Styles';
 
 interface Props extends StackScreenProps<RootAdminBottomTabParamList, 'ChangePasswordScreen'> {}
 
 const ChangePasswordScreen = ({ navigation }: Props) => {
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  // Función para manejar el cambio de contraseña
+  const handleChangePassword = () => {
+    // Aquí puedes añadir la lógica para cambiar la contraseña
+  };
+
   return (
-    <View>
-      <Text>ChangePasswordScreen</Text>
+    <View style={ProfileInfoStyles.profileContainer}>
+      <Text style={ProfileInfoStyles.mainText}>Cambiar contraseña</Text>
+      <View style={ProfileInfoStyles.fieldContainer}>
+        <TextInput
+          style={ProfileInfoStyles.textInputContainer}
+          placeholder="Nueva contraseña"
+          secureTextEntry={true}
+          value={newPassword}
+          onChangeText={(text) => setNewPassword(text)}
+        />
+        <TextInput
+          style={ProfileInfoStyles.textInputContainer}
+          placeholder="Confirmar contraseña"
+          secureTextEntry={true}
+          value={confirmPassword}
+          onChangeText={(text) => setConfirmPassword(text)}
+        />
+      </View>
+      <Pressable onPress={handleChangePassword}>
+        <Text style={ProfileInfoStyles.buttomChangePassword}>Cambiar contraseña</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default ChangePasswordScreen
-
-const styles = StyleSheet.create({})
+export default ChangePasswordScreen;
