4b119503f5c0e9eefc58f1d376930ae02b7dab91
tortaaaa <pablo.reyes01@alumnos.ucn.cl>
2024-05-14 17:43:14 -0400

    feat: frontend validations for new password and confirm new password (PARTIALLY IMPLEMENTED)

--- src/Presentation/screens/profile/changePassword/ChangePasswordScreen.tsx
+++ src/Presentation/screens/profile/changePassword/ChangePasswordScreen.tsx
@@ -3,35 +3,67 @@ import { View, Text, TextInput, Pressable } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootAdminBottomTabParamList } from '../../../navigation/tabs/admin/AdminBottomTab';
 import ChangePasswordStyles from './Styles';
+import useViewModel from './ViewModel';
 
 interface Props extends StackScreenProps<RootAdminBottomTabParamList, 'ChangePasswordScreen'> {}
 
 const ChangePasswordScreen = ({ navigation }: Props) => {
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  // Función para manejar el cambio de contraseña
-  const handleChangePassword = () => {
-    // Aquí puedes añadir la lógica para cambiar la contraseña
+  const { onChange, isValidForm, loading, hasEightChars, hasUppercase, hasNumber, hasSpecialChar, errorMessages } = useViewModel();
+
+  const handleChangePassword = async () => {
+    const isValid = await isValidForm();
+
+    if (isValid) {
+      // Lógica para cambiar la contraseña
+    }
   };
 
   return (
     <View style={ChangePasswordStyles.changePasswordContainer}>
       <Text style={ChangePasswordStyles.mainText}>Cambiar contraseña</Text>
       <View style={ChangePasswordStyles.fieldContainer}>
-        <TextInput
-          style={ChangePasswordStyles.textInputContainer}
-          placeholder="Nueva contraseña"
-          secureTextEntry={true}
-          value={newPassword}
-          onChangeText={(text) => setNewPassword(text)}
-        />
-        <TextInput
-          style={ChangePasswordStyles.textInputContainer}
-          placeholder="Confirmar contraseña"
-          secureTextEntry={true}
-          value={confirmPassword}
-          onChangeText={(text) => setConfirmPassword(text)}
-        />
+        <View style={ChangePasswordStyles.fieldContainer}>
+          <TextInput
+            style={ChangePasswordStyles.textInputContainer}
+            placeholder="Nueva contraseña"
+            secureTextEntry={true}
+            value={newPassword}
+            onChangeText={(text) => {
+              setNewPassword(text);
+              onChange('password', text);
+            }}
+          />
+          {errorMessages.password && <Text style={ChangePasswordStyles.errorText}>{errorMessages.password}</Text>}
+        </View>
+        <View style={ChangePasswordStyles.passwordRequerimentContainer}>
+          <Text style={hasEightChars ? ChangePasswordStyles.completed : ChangePasswordStyles.uncompleted}>
+            * Al menos 8 caracteres
+          </Text>
+          <Text style={hasUppercase ? ChangePasswordStyles.completed : ChangePasswordStyles.uncompleted}>
+            * Al menos una mayúscula
+          </Text>
+          <Text style={hasNumber ? ChangePasswordStyles.completed : ChangePasswordStyles.uncompleted}>
+            * Al menos un número
+          </Text>
+          <Text style={hasSpecialChar ? ChangePasswordStyles.completed : ChangePasswordStyles.uncompleted}>
+            * Al menos un caracter especial
+          </Text>
+        </View>
+
+
+          <TextInput
+            style={ChangePasswordStyles.textInputContainer}
+            placeholder="Confirmar Contraseña"
+            secureTextEntry={true}
+            value={confirmPassword}
+            onChangeText={(text) => {
+              setNewPassword(text);
+              onChange('confirmPassword', text);
+            }}
+          />
+          {errorMessages.confirmPassword && <Text style={ChangePasswordStyles.errorText}>{errorMessages.confirmPassword}</Text>}
       </View>
       <Pressable onPress={handleChangePassword}>
         <Text style={ChangePasswordStyles.buttomChangePassword}>Cambiar contraseña</Text>
