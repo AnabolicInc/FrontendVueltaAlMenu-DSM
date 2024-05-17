01707a849b2054ae463ffb2288142ca1882081cb
tortaaaa <pablo.reyes01@alumnos.ucn.cl>
2024-05-17 15:21:45 -0400

    feat ChangePassword repository logic implemented

--- src/Data/repositories/ChangePasswordRepository.tsx
+++ src/Data/repositories/ChangePasswordRepository.tsx
@@ -2,32 +2,33 @@ import { AxiosError } from "axios";
 import { ChangePasswordRepository } from "../../Domain/repositories/ChangePasswordRepository";
 import { ApiDelivery } from "../sources/remote/api/ApiDelivery";
 import { ResponseAPIDelivery } from "../sources/remote/api/models/ResponseApiDelivery";
-console.log("Actualmente en ChangePasswordRepository")
 
 export class ChangePasswordRepositoryImpl implements ChangePasswordRepository {
     
     async update(email: string, newPassword: string, session_token: string): Promise<ResponseAPIDelivery> {
-        console.log("Antes de ChangePasswordRepository.Update.try")
+        console.log("Antes de ChangePasswordRepository.Update.try");
+
+        // Declarar la variable de prueba
 
         try {
             // Define la ruta de la solicitud al backend
             const path = `user/new-password`;
-            console.log("Actualmente en ChangePasswordRepository.Update.try")
+            console.log("Actualmente en ChangePasswordRepository.Update.try");
 
             // Realiza la solicitud al backend utilizando Axios
-            const { data } = await ApiDelivery.put<ResponseAPIDelivery>(path, {email, newPassword},{
+            const { data } = await ApiDelivery.post<ResponseAPIDelivery>(path, { email, newPassword}, {
                 headers: {
                     'Authorization': `Bearer ${session_token}`
                 }
             });
-            console.log("Actualmente terminó la solicitud")
+            console.log("Actualmente terminó la solicitud");
 
             // Devuelve los datos recibidos del backend
             return Promise.resolve(data);
         } catch (error) {
             // Maneja cualquier error que ocurra durante la solicitud
             let e = error as AxiosError;
-            const apiError: ResponseAPIDelivery = JSON.parse(JSON.stringify(e.response.data));
+            const apiError: ResponseAPIDelivery = JSON.parse(JSON.stringify(e.response?.data));
             return Promise.reject(apiError);
         }
     }
