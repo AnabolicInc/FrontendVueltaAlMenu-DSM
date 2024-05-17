c2a7f7a0d24126456dcda5584208be0574ca57d5
Royman Valencia <royman.valencia@alumnos.ucn.cl>
2024-05-17 02:22:56 -0400

    style(categoryList): modified category list styles

--- src/Presentation/screens/admin/category/list/Styles.tsx
+++ src/Presentation/screens/admin/category/list/Styles.tsx
@@ -15,7 +15,7 @@ const CategoryListStyles = StyleSheet.create(
 
       categoryListText: {
         position: 'absolute',
-        top: "15%",
+        top: "10%",
         color: COLORS.primaryWhite,
         fontFamily: 'Poppins',
         fontSize: FONTSIZE.size_20,
@@ -26,11 +26,11 @@ const CategoryListStyles = StyleSheet.create(
 
       buttonAdd: {
         position: 'absolute',
-        top: "87%",
+        top: "92%",
         backgroundColor: COLORS.primaryOrange,
         borderRadius: 10,
         width: 150,
-        height: 45,
+        height: 45, 
       },
      
       saveText: {
@@ -45,9 +45,9 @@ const CategoryListStyles = StyleSheet.create(
         flexGrow: 1,
         flexDirection: 'column',
         width: "85%",
-        height: "52%",
+        height: "70%",
         position: 'absolute',
-        top: "23%",
+        top: "18%",
         alignContent: 'center',
         borderRadius: 10,
       },
@@ -56,7 +56,7 @@ const CategoryListStyles = StyleSheet.create(
         //backgroundColor: COLORS.primaryGrey,
         flex: 1,
         width: "100%",
-        height: 140,
+        height: 150,
         borderRadius: 30,
         marginBottom: 10,
         paddingStart: 15,
@@ -73,7 +73,7 @@ const CategoryListStyles = StyleSheet.create(
       categoryListInnerInnerElement: {
         alignSelf: 'stretch',
         flexDirection: 'row',
-        top: 40,
+        top: 25,
       },
 
       categoryListImage: {
@@ -81,16 +81,24 @@ const CategoryListStyles = StyleSheet.create(
         borderRadius: 30,
         width: 100,
         height: 100,
-        top: 16,
+        top: 20,
         marginRight: '4%',
       },
 
 
-      categoryListElementText: {
+      categoryListElementTittle: {
         color: COLORS.primaryWhite,
         fontFamily: 'Poppins',
         textAlign: 'left',
-        fontSize: FONTSIZE.size_18  ,
+        fontSize: FONTSIZE.size_16  ,
+        top: 17,
+      },
+
+      categoryListElementDescription: {
+        color: COLORS.primaryLightGrey,
+        fontFamily: 'Poppins',
+        textAlign: 'left',
+        fontSize: FONTSIZE.size_12  ,
         top: 17,
       },
 
