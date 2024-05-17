ad2a7bcd28f6c90b50b48917a729ea47369bdd44
Royman Valencia <royman.valencia@alumnos.ucn.cl>
2024-05-17 02:19:59 -0400

    fix(categoryList): category list refresh fixed

--- src/Presentation/screens/admin/category/list/ViewModel.tsx
+++ src/Presentation/screens/admin/category/list/ViewModel.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CategoryListUseCase } from "../../../../../Domain/useCases/Category/CategoryListUseCase";
 import { Category } from "../../../../../Domain/entities/Category";
+import { useFocusEffect } from "@react-navigation/native";
 
 const CategoryListViewModel = () => {
-
     const [categories, setCategories] = useState<Category[]>([]);
 
-    useEffect(() => {
-        getCategories()
-    }, [])
+    useEffect (() => { 
+        getCategories();
+    });
     
     const getCategories = async () => {
 
