4dee1847c34063b06fafe6c438869f0873eb6090
Royman Valencia <royman.valencia@alumnos.ucn.cl>
2024-05-26 19:39:13 -0400

    docs: file use cases documented

--- src/Domain/useCases/File/UpdateFileUseCase.tsx
+++ src/Domain/useCases/File/UpdateFileUseCase.tsx
@@ -1,7 +1,13 @@
 import { FileRepositoryImpl } from "../../../Data/repositories/FileRepository";
 import * as ImagePicker from 'expo-image-picker';
+
+// calls the updateFile method from the FileRepositoryImpl.
 const { updateFile } = new FileRepositoryImpl();
 
+/**
+ * Use case for the update operation.
+ */
 export const UpdateFileUseCase = async (file: ImagePicker.ImageInfo, collection: string, id: string) => {
+    // Uses the updateFile method from the FileRepositoryImpl.
     return await updateFile(file, collection, id);
 }
--- src/Domain/useCases/File/UpdateFilesUseCase.tsx
+++ src/Domain/useCases/File/UpdateFilesUseCase.tsx
@@ -1,7 +1,13 @@
 import { FileRepositoryImpl } from "../../../Data/repositories/FileRepository";
 import * as ImagePicker from 'expo-image-picker';
+
+// calls the updateFiles method from the FileRepositoryImpl.
 const { updateFiles } = new FileRepositoryImpl();
 
+/**
+ * Use case for the update operation.
+ */
 export const UpdateFilesUseCase = async (files: ImagePicker.ImageInfo[], collection: string, id: string) => {
+    // Uses the updateFiles method from the FileRepositoryImpl.
     return await updateFiles(files, collection, id);
 }
