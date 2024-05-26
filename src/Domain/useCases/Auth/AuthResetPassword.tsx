b75cb6fcb7983b6e0016484b82a7b9c65b894b1b
Royman Valencia <royman.valencia@alumnos.ucn.cl>
2024-05-26 19:34:15 -0400

    docs: auth use cases documented

--- src/Domain/useCases/Auth/AuthResetPassword.tsx
+++ src/Domain/useCases/Auth/AuthResetPassword.tsx
@@ -1,7 +1,12 @@
 import { AuthRepositoryImpl } from "../../../Data/repositories/AuthRepository";
 
+// Calls the forgotPassword method from the AuthRepositoryImpl.
 const { forgotPassword } = new AuthRepositoryImpl();
 
+/**
+ * Use case for the reset password operation.
+ */
 export const forgotPasswordAuthUseCase = async (email: string) => {
+    // Uses the forgotPassword method from the AuthRepositoryImpl.
     return await forgotPassword(email);
 }
--- src/Domain/useCases/Auth/AuthVerifyCode.tsx
+++ src/Domain/useCases/Auth/AuthVerifyCode.tsx
@@ -1,7 +1,12 @@
 import { AuthRepositoryImpl } from "../../../Data/repositories/AuthRepository";
 
+// Calls the verifyCode method from the AuthRepositoryImpl.
 const { verifyCode } = new AuthRepositoryImpl()
 
+/**
+ * Use case for the verify code operation.
+ */
 export const verifyCodeAuthUseCase = async (email: string, code: string) => {
+    // Uses the verifyCode method from the AuthRepositoryImpl.
     return await verifyCode(email, code);
 }
