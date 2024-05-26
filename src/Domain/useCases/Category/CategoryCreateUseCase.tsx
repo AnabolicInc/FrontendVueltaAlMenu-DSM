0576ae36e334c3795d89867e8ac82eaeba3da3d4
Royman Valencia <royman.valencia@alumnos.ucn.cl>
2024-05-26 19:34:56 -0400

    docs: category use cases documented

--- src/Domain/useCases/Category/CategoryCreateUseCase.tsx
+++ src/Domain/useCases/Category/CategoryCreateUseCase.tsx
@@ -1,8 +1,13 @@
 import { CategoryRepositoryImpl } from "../../../Data/repositories/CategoryRepository";
 import { Category } from "../../entities/Category";
 
+// Calls the create method from the CategoryRepositoryImpl.
 const { create } = new CategoryRepositoryImpl();
 
+/**
+ * Use case for the create operation.
+ */
 export const CategoryCreateUseCase = async (category: Category) => {
+    // Uses the create method from the CategoryRepositoryImpl.
     return await create(category);
 }
--- src/Domain/useCases/Category/CategoryDeleteUseCase.tsx
+++ src/Domain/useCases/Category/CategoryDeleteUseCase.tsx
@@ -1,8 +1,12 @@
-
 import { CategoryRepositoryImpl } from "../../../Data/repositories/CategoryRepository";
 
+// Calls the delete method from the CategoryRepositoryImpl.
 const { delete : deleteCategory } = new CategoryRepositoryImpl();
 
+/**
+ * Use case for the delete operation.
+ */
 export const CategoryDeleteUseCase = async (id: string) => {
+    // Uses the delete method from the CategoryRepositoryImpl.
     return await deleteCategory(id);
 }
