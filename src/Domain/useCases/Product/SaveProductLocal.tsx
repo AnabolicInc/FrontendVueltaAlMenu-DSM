baa294c2d7e8f2f1300c98a7c4dfdaff026567da
Royman Valencia <royman.valencia@alumnos.ucn.cl>
2024-05-26 19:40:35 -0400

    docs: product use cases documented

--- src/Domain/useCases/Product/SaveProductLocal.tsx
+++ src/Domain/useCases/Product/SaveProductLocal.tsx
@@ -1,8 +1,13 @@
 import { ProductLocalRepositoryImpl } from "../../../Data/repositories/ProductLocalRepository";
 import { Product } from "../../entities/Product";
 
+// Calls the save method from the ProductLocalRepositoryImpl.
 const { save } = new ProductLocalRepositoryImpl();
 
+/**
+ * Use case for the save operation.
+ */
 export const SaveProductUseCase = async (product: Product) => {
+    // Uses the save method from the ProductLocalRepositoryImpl.
     return await save(product);
 }
