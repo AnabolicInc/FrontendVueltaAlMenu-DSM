964cc29a9d2c7dfd438d2e7e6b1e18cc5cd50f09
Royman Valencia <royman.valencia@alumnos.ucn.cl>
2024-05-26 17:49:01 -0400

    docs: Data repositories documented

--- src/Data/repositories/ProductRepository.tsx
+++ src/Data/repositories/ProductRepository.tsx
@@ -4,16 +4,25 @@ import { ProductRepository } from "../../Domain/repositories/ProductRepository";
 import { ApiDelivery } from "../sources/remote/api/ApiDelivery";
 import { ResponseAPIDelivery } from "../sources/remote/api/models/ResponseApiDelivery";
 
+/**
+ * Class to implement the ProductRepository interface
+ */
 export class ProductRepositoryImpl implements ProductRepository {
 
+    /**
+     * Method to send a request to the server to create a product
+     * @param product is the product to be created
+     * @returns the response from the server
+     */
     async create(product: Product): Promise<ResponseAPIDelivery> {
         try {
+            // Call the post method from the ApiDelivery class to send a createProduct request to the server
             const { data } = await ApiDelivery.post<ResponseAPIDelivery>('product/createProduct', product);
             console.log('DATA: ', JSON.stringify(data));
-            
+            // Return the response from the server
             return Promise.resolve(data)
-
         } catch (error) {
+            // Handle any error that occurs during the request
             let e = (error as AxiosError);
             if (e.response) {
                 console.log('ERROR: ', JSON.stringify(e.response.data));
--- src/Data/repositories/UserUpdateRepository.tsx
+++ src/Data/repositories/UserUpdateRepository.tsx
@@ -3,26 +3,24 @@ import { UserUpdateRepository } from "../../Domain/repositories/UserUpdateReposi
 import { ApiDelivery } from "../sources/remote/api/ApiDelivery";
 import { ResponseAPIDelivery } from "../sources/remote/api/models/ResponseApiDelivery";
 
-
-
-
+/**
+ * Class to implement the UserUpdateRepository interface
+ */
 export class UserUpdateRepositoryImpl implements UserUpdateRepository {
     async update (id: string, name: string, lastName: string, phone: string, session_token: string): Promise<ResponseAPIDelivery> {
-        //console.log('CHANGES FROM THE USER',id, name, lastName, phone, session_token);
-        
         try{
-            const path = `user/${id}`;
-            //console.log('PATH', path);
-            
-
+            // Define the path of the request
+            const path = `user/${id}`;           
+            // Call the put method from the ApiDelivery class to send a userUpdate request to the server
             const {data} = await ApiDelivery.put<ResponseAPIDelivery>(path, {name, lastName, phone},{
                 headers: {
                     'Authorization': `Bearer ${session_token}`
                 }
             });
-
+            // Return the response from the server
             return Promise.resolve(data);
         } catch (error) {
+            // Handle any error that occurs during the request
             let e = (error as AxiosError);
             const apiError: ResponseAPIDelivery = JSON.parse(JSON.stringify(e.response.data));
             return Promise.reject(apiError);
