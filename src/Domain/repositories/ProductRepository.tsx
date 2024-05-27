0476d81e320169320e858919e593faf79040fc70
Giantt2 <giantt67@gmail.com>
2024-05-27 17:21:20 -0400

    feat(Product): ProductContext implemented

--- src/Domain/repositories/ProductRepository.tsx
+++ src/Domain/repositories/ProductRepository.tsx
@@ -4,5 +4,8 @@ import { Product } from "../entities/Product";
 
 
 export interface ProductRepository {
-    create(pruduct: Product): Promise<ResponseAPIDelivery>
+    getAllProducts(id: string): Promise<ResponseAPIDelivery>;
+    createProduct(product: Product): Promise<ResponseAPIDelivery>
+    updateProduct(id: string, name: string, description: string, price: number, quantity: number): Promise<ResponseAPIDelivery>
+    deleteProduct(id: string): Promise<ResponseAPIDelivery>
 }
--- src/Domain/useCases/Product/ProductCreateUseCase.tsx
+++ src/Domain/useCases/Product/ProductCreateUseCase.tsx
@@ -1,12 +1,8 @@
 import { ProductRepositoryImpl } from "../../../Data/repositories/ProductRepository";
 import { Product } from "../../entities/Product";
 
-const { create } = new ProductRepositoryImpl();
+const { createProduct } = new ProductRepositoryImpl();
 
 export const ProductCreateUseCase = async (product: Product) => {
-    return await create(product);
-}
-
-export const getAllProducts = async () => {
-    return await getAllProducts();
+    return await createProduct(product);
 }
