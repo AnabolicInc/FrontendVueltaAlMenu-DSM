ed783adaa2dcbb020b997851b27771e16d53cf9e
tortaaaa <pablo.reyes01@alumnos.ucn.cl>
2024-05-29 13:20:44 -0400

    refactor: not necesary console.log line deleted

--- src/Domain/useCases/User/ChangePasswordUseCase.tsx
+++ src/Domain/useCases/User/ChangePasswordUseCase.tsx
@@ -1,7 +1,6 @@
 import { ChangePasswordRepositoryImpl } from "../../../Data/repositories/ChangePasswordRepository";
 
 const { update } = new ChangePasswordRepositoryImpl();
-console.log("Actualmente en ChangePasswordUseCase")
 
 export const ChangePasswordUseCase = async (email: string, newPassword: string, session_token: string) => {
     return await update (email, newPassword, session_token);
