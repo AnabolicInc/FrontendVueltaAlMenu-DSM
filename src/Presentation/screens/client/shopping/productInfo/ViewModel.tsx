e164d82fe4b09320843d29f652bafec28414c412
tortaaaa <pablo.reyes01@alumnos.ucn.cl>
2024-06-16 23:23:24 -0400

    feat: buttons for add and substract items added

--- src/Presentation/screens/client/shopping/productInfo/ViewModel.tsx
+++ src/Presentation/screens/client/shopping/productInfo/ViewModel.tsx
@@ -1,26 +1,20 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { Product } from "../../../../../Domain/entities/Product";
-import { ShoppingCartContext } from "../../../../context/shopping/ShoppingCartContext";
-
+import { ProductShoppingContext } from "../../../../context/ProductShoppingContext";
 
 const ProductInfoViewModel = () => {
+  const { saveProductShoppingCart, addItem, subtracItem } = useContext(ProductShoppingContext);
 
+  const addToCart = async (product: Product) => {
+    console.log('Add to cart', product);
+    await saveProductShoppingCart(product); 
+  };
 
-    const { saveProductShoppingCart } = useContext(ShoppingCartContext);
-
-    const addToCart = async (product: Product) => {
-        console.log('Add to cart', product);
-        await saveProductShoppingCart(product); 
-    }
-
-
-    return {
-        addToCart
-
-    };
-
-
+  return {
+    addToCart,
+    addItem,
+    subtracItem,
+  };
 }
-
 
 export default ProductInfoViewModel;
